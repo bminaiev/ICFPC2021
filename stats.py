360195efe907d664e4087f12dbd8c61f3e34313a
Roman Udovichenko <mailromka@gmail.com>
2021-07-09 23:47:23 +0300

    more info

--- stats.py
+++ stats.py
@@ -5,6 +5,7 @@ import json
 cookies = "session=8e493aa7-3270-4cbd-adf6-6027d3eeeb53; spockcookie=Oksn_N6VJHIC3QiHLMpPrO8q7oahK8n-WK_AuEr4MqWjM1Q1U07tezwqU5TvJ4zbJjIiY-E8-iwLgvIEJ5iBRA"
 r = requests.get("https://poses.live/problems", headers={'cookie': cookies})
 total = 0
+total_best = 0
 for row in r.text.split("</tr><tr>"):
     if not row.startswith("<td>"):
         continue
@@ -22,8 +23,13 @@ for row in r.text.split("</tr><tr>"):
         coeff = math.log(holes * vertices * edges / 6.0) / math.log(2.0)
 
     score = math.ceil(1000 * coeff * math.sqrt((best + 1.0) / (our + 1.0)))
-    print("{0:3s} | {1:5d} | {2:5d} = {3}".format(test_id, our, best, score))
+    best_score = math.ceil(1000 * coeff)
+    loss = "- best!"
+    if best_score > score:
+        loss = "({0} lost)".format(best_score - score)
+    print("{0:3s} | {1:5d} | {2:5d} = {3:5d} of {4} {5}".format(test_id, our, best, score, best_score, loss))
     total += score
+    total_best += best_score
 
 print("=" * 30)
-print("Total: ", total)
+print("Total: {0} of {1}".format(total, total_best))
