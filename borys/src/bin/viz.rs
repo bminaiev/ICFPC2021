b62a7971791e5311eadf01a308222daae09908ad
Borys Minaiev <bminaiev@gmail.com>
2021-07-10 16:37:44 +0100

    do not wait

--- borys/src/bin/viz.rs
+++ borys/src/bin/viz.rs
@@ -223,7 +223,7 @@ pub fn main() {
         }
         // The rest of the game loop goes here...
 
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 600));
+        // ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 600));
     }
 }
 
